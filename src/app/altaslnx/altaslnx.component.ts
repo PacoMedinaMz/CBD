43655aadac3eddcd0bcbaa1d9d57d34e4fe91904
PacoMedinaMz <pacomedina.05.12.99@outlook.com>
2021-06-17 00:11:09 -0500

    Base de las altas, tanto para win, como para lnx. Solo falta la api y enviar

--- src/app/altaslnx/altaslnx.component.ts
+++ src/app/altaslnx/altaslnx.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-altaslnx',
@@ -7,6 +9,31 @@ import Swal from 'sweetalert2';
   styleUrls: ['./altaslnx.component.scss']
 })
 export class AltaslnxComponent implements OnInit {
+
+  asgForm = new FormGroup({
+    eno: new FormControl(''),
+    pno: new FormControl(''),
+    resp: new FormControl(''),
+    dur: new FormControl('')
+  });
+
+  empForm = new FormGroup({
+    eno: new FormControl(''),
+    ename: new FormControl(''),
+    title: new FormControl('')
+  });
+
+  payForm = new FormGroup({
+    payno: new FormControl(''),
+    title: new FormControl(''),
+    sal: new FormControl('')
+  });
+
+  projForm = new FormGroup({
+    pno: new FormControl(''),
+    pname: new FormControl(''),
+    budget: new FormControl('')
+  });
 
   constructor() {
 
@@ -36,5 +63,97 @@ export class AltaslnxComponent implements OnInit {
       timer: 1500
     })
   }
-  
+
+  clickEnviarAsg () {
+    var e = this.asgForm.value.eno;
+    var p = this.asgForm.value.pno;
+    var resp = this.asgForm.value.resp;
+    var dur = this.asgForm.value.dur;
+
+    var eno = "E";
+    eno += e.toString();
+    var pno = "P";
+    pno += p.toString();
+
+    var asgJson =
+    {
+      "eno": eno,
+      "pno": pno,
+      "resp": resp,
+      "dur": dur
+    };
+
+    console.log(asgJson);
+  }
+
+  clickEnviarEmp () {
+    var e = this.empForm.value.emp;
+    var ename = this.empForm.value.ename;
+    var title = this.empForm.value.title;;
+
+    var eno = "E";
+    eno += e.toString();
+
+    var enoJson =
+    {
+      "eno": eno,
+      "ename": ename,
+      "title": title
+    };
+
+    console.log(enoJson);
+  }
+
+  clickEnviarPay () {
+    var p = this.payForm.value.payno;
+    var title = this.payForm.value.title;
+    var sal = this.payForm.value.sal;
+
+    var payno = "Py";
+    payno += p.toString();
+
+    var payJson =
+    {
+      "payno": payno,
+      "title": title,
+      "sal": sal
+    };
+
+    console.log(payJson);
+  }
+
+  clickEnviarProj () {
+    var p = this.projForm.value.pno;
+    var pname = this.projForm.value.pname;
+    var budget = this.projForm.value.budget;
+
+    var pno = "P";
+    pno += p.toString();
+
+    var projJson =
+    {
+      "pno": pno,
+      "pname": pname,
+      "budget": budget
+    };
+
+    console.log(projJson);
+  }
+
+  enviar(){
+    /*
+    this.http.post<any>('http://m.pacomedina.mx:4201/crud/final', jPuesto).subscribe({
+      next: data => {
+        if (data.status === 'error') {
+          this.sendError("No se pudo generar reporte a causa de error del servidor.");
+        } else {
+          this.registerSuccess();
+        }
+
+      },
+      error: error => {
+        console.error('ERROR al generar reporte', error.message);
+      }
+    })*/
+  }
 }
--- src/app/altaswin/altaswin.component.ts
+++ src/app/altaswin/altaswin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-altaswin',
@@ -8,7 +9,33 @@ import Swal from 'sweetalert2';
 })
 export class AltaswinComponent implements OnInit {
 
+  asgForm = new FormGroup({
+    eno: new FormControl(''),
+    pno: new FormControl(''),
+    resp: new FormControl(''),
+    dur: new FormControl('')
+  });
+
+  empForm = new FormGroup({
+    eno: new FormControl(''),
+    ename: new FormControl(''),
+    title: new FormControl('')
+  });
+
+  payForm = new FormGroup({
+    payno: new FormControl(''),
+    title: new FormControl(''),
+    sal: new FormControl('')
+  });
+
+  projForm = new FormGroup({
+    pno: new FormControl(''),
+    pname: new FormControl(''),
+    budget: new FormControl('')
+  });
+
   constructor() {
+    console.log("Hola");
 
   }
 
@@ -35,4 +62,96 @@ export class AltaswinComponent implements OnInit {
     })
   }
 
+  clickEnviarAsg () {
+    var e = this.asgForm.value.eno;
+    var p = this.asgForm.value.pno;
+    var resp = this.asgForm.value.resp;
+    var dur = this.asgForm.value.dur;
+
+    var eno = "E";
+    eno += e.toString();
+    var pno = "P";
+    pno += p.toString();
+
+    var asgJson =
+    {
+      "eno": eno,
+      "pno": pno,
+      "resp": resp,
+      "dur": dur
+    };
+
+    console.log(asgJson);
+  }
+
+  clickEnviarEmp () {
+    var e = this.empForm.value.emp;
+    var ename = this.empForm.value.ename;
+    var title = this.empForm.value.title;;
+
+    var eno = "E";
+    eno += e.toString();
+
+    var enoJson =
+    {
+      "eno": eno,
+      "ename": ename,
+      "title": title
+    };
+
+    console.log(enoJson);
+  }
+
+  clickEnviarPay () {
+    var p = this.payForm.value.payno;
+    var title = this.payForm.value.title;
+    var sal = this.payForm.value.sal;
+
+    var payno = "Py";
+    payno += p.toString();
+
+    var payJson =
+    {
+      "payno": payno,
+      "title": title,
+      "sal": sal
+    };
+
+    console.log(payJson);
+  }
+
+  clickEnviarProj () {
+    var p = this.projForm.value.pno;
+    var pname = this.projForm.value.pname;
+    var budget = this.projForm.value.budget;
+
+    var pno = "P";
+    pno += p.toString();
+
+    var projJson =
+    {
+      "pno": pno,
+      "pname": pname,
+      "budget": budget
+    };
+
+    console.log(projJson);
+  }
+
+  enviar(){
+    /*
+    this.http.post<any>('http://m.pacomedina.mx:4201/crud/final', jPuesto).subscribe({
+      next: data => {
+        if (data.status === 'error') {
+          this.sendError("No se pudo generar reporte a causa de error del servidor.");
+        } else {
+          this.registerSuccess();
+        }
+
+      },
+      error: error => {
+        console.error('ERROR al generar reporte', error.message);
+      }
+    })*/
+  }
 }
