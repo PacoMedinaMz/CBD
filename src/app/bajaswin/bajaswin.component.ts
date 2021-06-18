93028e0464001302af63e808c8950b15c5f20257
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-06-17 20:07:20 -0500

    Bajas en windows

--- src/app/bajaswin/bajaswin.component.ts
+++ src/app/bajaswin/bajaswin.component.ts
@@ -1,62 +1,155 @@
-import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import Swal from "sweetalert2";
+import { FormGroup, FormControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-bajaswin',
-  templateUrl: './bajaswin.component.html',
-  styleUrls: ['./bajaswin.component.scss']
+  selector: "app-bajaswin",
+  templateUrl: "./bajaswin.component.html",
+  styleUrls: ["./bajaswin.component.scss"],
 })
 export class BajaswinComponent implements OnInit {
+  formAsg = new FormGroup({
+    id: new FormControl(""),
+  });
+  formEmp = new FormGroup({
+    id: new FormControl(""),
+  });
+  formPay = new FormGroup({
+    id: new FormControl(""),
+  });
+  formProj = new FormGroup({
+    id: new FormControl(""),
+  });
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  warning() {
+  warning(functionCallback) {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
       },
-      buttonsStyling: false
-    })
+      buttonsStyling: false,
+    });
 
-    swalWithBootstrapButtons.fire({
-      title: '¿Estás seguro de borrarlo?',
-      text: "¡No podrás deshacer este cambio!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, borralo!',
-      cancelButtonText: 'No, cancelar!',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        swalWithBootstrapButtons.fire(
-          'Borrado!',
-          'Los datos han sido borrados.',
-          'success'
-        )
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire(
-          'Cancelado',
-          'Los datos permanecen en la BD',
-          'error'
-        )
-      }
-    })
+    let thas = this;
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "¿Estás seguro de borrarlo?",
+        text: "¡No podrás deshacer este cambio!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, borralo!",
+        cancelButtonText: "No, cancelar!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          functionCallback(thas);
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            "Cancelado",
+            "Los datos permanecen en la BD",
+            "error"
+          );
+        }
+      });
+  }
+
+  clickEnviarAsg(thas) {
+    //Usa thas en vez de this
+    var id = thas.formAsg.value.id;
+
+    thas.enviarPeticion(
+      id,
+      "http://192.168.1.76:3003/windows/bajaAsg",
+      "http://192.168.1.76:3003/windows/bajaAsgR"
+    );
+  }
+
+  clickEnviarEmp(thas) {
+    //Usa thas en vez de this
+    var id = thas.formEmp.value.id;
+
+    thas.enviarPeticion(
+      id,
+      "http://192.168.1.76:3003/windows/bajaEmp",
+      "http://192.168.1.76:3003/windows/bajaEmpR"
+    );
+  }
+
+  clickEnviarPay(thas) {
+    //Usa thas en vez de this
+    var id = thas.formPay.value.id;
+
+    thas.enviarPeticion(
+      id,
+      "http://192.168.1.76:3003/windows/bajaPay",
+      "http://192.168.1.76:3003/windows/bajaPayR"
+    );
+  }
+
+  clickEnviarProj(thas) {
+    //Usa thas en vez de this
+    var id = thas.formProj.value.id;
+
+    thas.enviarPeticion(
+      id,
+      "http://192.168.1.76:3003/windows/bajaProj",
+      "http://192.168.1.76:3003/windows/bajaProjR"
+    );
+  }
+
+  enviarPeticion(id, url, url2) {
+    var json = {
+      id: id,
+    };
+
+    console.log(json);
+
+    this.sendHttp(json, url);
+    this.sendHttp(json, url2);
+  }
+
+  sendHttp(json, url) {
+    this.http.post<any>(url, json).subscribe({
+      next: (data) => {
+        if (data.status === "error") {
+          this.error();
+        } else {
+          this.success();
+        }
+      },
+      error: (error) => {
+        console.error("ERROR al generar reporte", error.message);
+      },
+    });
   }
 
   error() {
     Swal.fire({
-      position: 'center',
-      icon: 'error',
-      title: '¡los datos no han sido eliminados!',
+      position: "center",
+      icon: "error",
+      title: "¡los datos no han sido eliminados!",
       showConfirmButton: false,
-      timer: 1500
-    })
+      timer: 1500,
+    });
+  }
+
+  success() {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "¡Datos eliminados correctamente!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   }
 }
